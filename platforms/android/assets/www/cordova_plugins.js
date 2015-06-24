0f9a4d401520d4fa71b2feca34b2e0572eba39c4
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2015-06-24 16:52:36 -0300

    Exams App
    Project Init

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,23 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.phonegap.plugins.facebookconnect/facebookConnectPlugin.js",
+        "id": "com.phonegap.plugins.facebookconnect.FacebookConnectPlugin",
+        "clobbers": [
+            "facebookConnectPlugin"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.phonegap.plugins.facebookconnect": "0.11.0",
+    "cordova-plugin-whitelist": "1.1.0"
+}
+// BOTTOM OF METADATA
+});
